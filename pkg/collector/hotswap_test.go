57705a8171b388c842efae0a51a6049d6a33e688
cstahl1 <cstahl1@users.noreply.github.com>
2018-05-11 12:50:39 -0400

    Add a threadsafe hot-swappable pipeline
    The hot-swappable pipeline is needed for when a pipeline configuration
    changes while the collector is running. By using this wrapper around a
    pipeline, a user can asyncronously change the pipeline being used with
    no interruptions to the request handling.
    
    Issue: #24

---
+++ pkg/collector/hotswap_test.go
@@ -0,0 +1,53 @@
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package collector_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/google/nel-collector/pkg/collector"
+)
+
+func TestHotSwapChangesWhichHandlerIsRun(t *testing.T) {
+	codeOkHandler := dummyHandler{http.StatusOK}
+	codeBadRequestHandler := dummyHandler{http.StatusBadRequest}
+
+	var hs collector.HotSwap
+	hs.Swap(codeOkHandler)
+
+	response := httptest.NewRecorder()
+	hs.ServeHTTP(response, &http.Request{})
+	if want := http.StatusOK; response.Code != want {
+		t.Errorf("ServeHTTP: got %d, wanted %d", response.Code, want)
+	}
+
+	hs.Swap(codeBadRequestHandler)
+
+	response = httptest.NewRecorder()
+	hs.ServeHTTP(response, &http.Request{})
+	if want := http.StatusBadRequest; response.Code != want {
+		t.Errorf("ServeHTTP: got %d, wanted %d", response.Code, want)
+	}
+}
+
+type dummyHandler struct {
+	statusCode int
+}
+
+func (d dummyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(d.statusCode)
+}
